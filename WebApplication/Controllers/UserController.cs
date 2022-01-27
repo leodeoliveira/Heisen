14e932a59c00bce7c319fd00786d80f99476e3a8
320097834 <320097834@NLYBSTQVP5NB527>
2022-01-26 21:10:28 -0300

    Create model and consuming by postman

---
+++ WebApplication/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("HeisenAppCon"));
+            var allUsers = dbClient.GetDatabase("heisendb").GetCollection<User>("Users").AsQueryable();
+            return new JsonResult(allUsers);
+        }
+    }
+}
