7a4c6b9710f9aeb14dff54334f4dc6861234e8f5
320097834 <320097834@NLYBSTQVP5NB527>
2022-03-03 10:03:39 -0300

    Create heisen-site

---
+++ heisen-site/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="App">
+    <Header />
+    
+  </div>
+</template>
+
+<script>
+import './styles/global.css'
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
---
+++ heisen-site/src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+    <header>
+       <div class="header-logo-wrapper col-auto p-0">
+            <div class="logo-wrapper"><a href="index.html"><img class="img-fluid" src="../assets//logo.jpg" alt=""></a></div>
+            <div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle" data-feather="align-center"></i></div>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'Header-site'
+    }
+</script>
+
+<style scoped>
+    
+    header{
+        background-color: var(--color-background-nav);
+        width: 100%;
+        height: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    nav {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    #logo {
+        width: 130px; 
+
+    }
+
+    #menu-button {
+        width: 30px;
+    }
+
+    #menu-overlay {
+        position: 0;
+        top: 0;
+        left: 0;
+        width: 40%;
+        height: 100vh;
+        background-color: #000000;
+        opacity: 0.8;
+    }
+
+</style>
